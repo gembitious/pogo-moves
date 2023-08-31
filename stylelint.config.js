0e4a86899699e07dd7a731f2f079d3c685282952
gukho <gukho@liveconnect.co.kr>
2023-08-31 17:50:41 +0900

    mui, font 추가, lint 등 설정 변경

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,27 +1,26 @@
 module.exports = {
-  extends: ["stylelint-config-standard"],
-  ignoreFiles: ["**/*.js", "**/*.ts"],
+  extends: ['stylelint-config-standard'],
+  ignoreFiles: ['**/*.js', '**/*.ts'],
   rules: {
-    "color-hex-case": "lower",
-    "max-empty-lines": 2,
-    "no-duplicate-selectors": null,
-    "no-missing-end-of-source-newline": null,
-    "no-descending-specificity": null,
-    "selector-type-no-unknown": null,
-    "at-rule-empty-line-before": null,
-    "rule-empty-line-before": null,
-    "string-quotes": null,
-    "at-rule-no-unknown": null,
-    "property-no-vendor-prefix": true,
-    "max-nesting-depth": 4,
-    "selector-class-pattern":
-      "^([_a-zA-Z][a-zA-Z0-9]*)((__|_|-)[a-zA-Z0-9]+)*$",
-    "selector-id-pattern": "^([_a-zA-Z][a-zA-Z0-9]*)((__|_|-)[a-zA-Z0-9]+)*$",
-    "property-no-unknown": [
+    'no-duplicate-selectors': null,
+    'no-missing-end-of-source-newline': null,
+    'no-descending-specificity': null,
+    'selector-type-no-unknown': null,
+    'at-rule-empty-line-before': null,
+    'rule-empty-line-before': null,
+    'string-quotes': null,
+    'at-rule-no-unknown': null,
+    'import-notation': 'string',
+    'property-no-vendor-prefix': true,
+    'max-nesting-depth': 4,
+    'selector-class-pattern':
+      '^([_a-zA-Z][a-zA-Z0-9]*)((__|_|-)[a-zA-Z0-9]+)*$',
+    'selector-id-pattern': '^([_a-zA-Z][a-zA-Z0-9]*)((__|_|-)[a-zA-Z0-9]+)*$',
+    'property-no-unknown': [
       true,
       {
-        ignoreProperties: ["contain", "appearance", "/^font-/"],
+        ignoreProperties: ['contain', 'appearance', '/^font-/'],
       },
     ],
   },
-};
+}
