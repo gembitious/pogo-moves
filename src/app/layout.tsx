135a79e9c7c0decbc492ad577773dc322d394867
gukho <gukho@liveconnect.co.kr>
2023-09-20 11:31:45 +0900

    google adsense 스크립트 주석 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { NavigationBar } from '@components/NavigationBar'
 import '@styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
+// import Script from 'next/script'
 import { FC, ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,11 +21,11 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
           {children}
         </div>
       </body>
-      <Script
+      {/* <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2255003690265274"
         crossOrigin="anonymous"
-      ></Script>
+      ></Script> */}
     </html>
   )
 }
